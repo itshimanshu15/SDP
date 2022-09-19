6c0f9ae01a7be070574ebc4975acab9a167cce86
Himanshu <amaliyarhimanshu15@gmail.com>
2022-09-20 02:15:34 +0530

    5
    5

---
+++ Lab10/lab10_1_widget_life_cycle/android/app/src/main/kotlin/com/example/lab10_1_widget_life_cycle/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab10_1_widget_life_cycle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Lab10/lab10_2_worldtime_api/android/app/src/main/kotlin/com/example/lab10_2_worldtime_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab10_2_worldtime_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
